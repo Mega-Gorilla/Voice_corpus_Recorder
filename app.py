0d570fb21334477aa82dafdbafc72eadc07d06b6
Mega-Gorilla <hahahadesu@gmail.com>
2024-02-27 01:28:04 +0900

    タイトルアイコンの変更

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ import streamlit as st
 import time
 
 def main():
+    st.set_page_config(page_title='Voice Corpus Recorder',
+                       menu_items={
+                           'Get Help':'https://github.com/Mega-Gorilla/Voice_corpus_Recorder',
+                           'Report a bug':'https://github.com/Mega-Gorilla/Voice_corpus_Recorder/issues',
+                           'About':'''### Voice Corpus Reader v1.0
+
+Created by 猩々 博士
+'''
+                       },
+                       page_icon='🎙')
+    
     corpus_list = get_function_list('modules.corpus_dict')
     select_voiceCorpus = st.selectbox('1.ボイスコーパスの選択',corpus_list)
     corpus_dict = execute_function('modules.corpus_dict',select_voiceCorpus)
@@ -13,6 +24,7 @@ def main():
     corpus_num = st.number_input('3.選択されているコーパス番号',max_value=len(corpus_dict),min_value=1)
     
     for key,value in corpus_dict[corpus_num-1].items():
+        value = auto_add_furigana(value) # フリガナを自動追加します。
         value = value.replace('。', '。<BR>')
         value = value.replace('、', '、<BR>')
         value = value.replace('\n', '<BR>')
