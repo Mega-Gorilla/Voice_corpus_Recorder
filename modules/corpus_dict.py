126904aaa37382b759807ec4be6c00951c8e10f1
Mega-Gorilla <hahahadesu@gmail.com>
2024-03-02 10:32:12 +0900

    説明文の追記

--- modules/corpus_dict.py
+++ modules/corpus_dict.py
@@ -12,7 +12,6 @@ from config import file_path
 """
     
 def JVNV_Corpus()->dict:
-
     dict_list = []
     with open(f'{file_path.corpus_folder}\JVNV.csv', 'r', encoding='utf-8') as file:
         for line in file:
@@ -49,6 +48,21 @@ def ITA_Corpus_Recitation()->dict:
             new_dict = {key:value}
             emotion_dict.append(new_dict)
     return emotion_dict
+
+"""
+任意のコーパスを追加する場合、
+
+def 任意のコーパス名()->dict:
+    任意コーパスを読み込み以下のフォーマットに変換し、returnするコードを記載してください。
+    追加した関数は、他のpyファイルでの宣言等は不要です。本corpus_dictに追加した関数は自動的に読み込まれます。
+    追加後アプリケーションを起動し、コーパス選択肢に追加されていることを確認してください。
+        フォーマット:
+        [{name:text}....]
+        フォーマット例:
+        [{"EMOTION100_001":"えっ嘘でしょ。"},
+        {"EMOTION100_002":"シュヴァイツァーは見習うべき人間です。"},
+        {"EMOTION100_003":"デーヴィスさんはとても疲れているように見える。"}
+"""
 
 if __name__ == "__main__":
     print(ITA_Corpus_Recitation())
